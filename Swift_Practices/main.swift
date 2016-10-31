ce67d8f8e150ed3b4f8b1988531936ce85052c2b
charles <forc2dm@gmail.com>
2016-10-31 10:42:59 +0800

    variable defination and type conversion

--- Swift_Practices/main.swift
+++ Swift_Practices/main.swift
@@ -9,3 +9,34 @@
 import Foundation
 
 print("Hello, World!")
+
+//----- variable defination
+
+// define a const
+let nameConst = "charles"
+
+// define int const
+let intConst = 1024
+
+// define double const
+let implicitDouble = 70.0
+
+// define float const
+let floatConst: Float = 1024.0
+
+
+// define a var
+var nameVar = "charles"
+
+//----- type conversion
+
+let appName = "AwesomeApp"
+
+let appVersion = 1.2
+
+//print(appName + appVersion) error
+print(appName + String(appVersion))
+
+
+
+
